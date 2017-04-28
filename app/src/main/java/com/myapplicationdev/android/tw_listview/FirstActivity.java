7cf4d52d35f8cddc4390a22648062acf79112371
Juhan Jufri <15017395@myrp.edu.sh>
2017-04-28 15:49:23 +0800

    HI THERE STRANGER

--- app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
+++ app/src/main/java/com/myapplicationdev/android/tw_listview/FirstActivity.java
@@ -1,7 +1,9 @@
 package com.myapplicationdev.android.tw_listview;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -26,6 +28,16 @@ public class FirstActivity extends AppCompatActivity {
         al.add("Year 1");
         al.add("Year 2");
         al.add("Year 3");
+
+        lv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivity(i);
+            }
+        });
+
+
 
         aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, al);
         lv.setAdapter(aa);
